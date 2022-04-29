3eb346c6e6f6948e7867f12e4c17afa372958bea
aonikpaulalock <alockpaul9845@gmail.com>
2022-04-29 11:24:01 +0600

    client-Connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const app = express() ;
+const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -7,13 +8,29 @@ const port = process.env.PORT || 5000;
 // MiddleWare
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
 
 app.get("/", (req, res) => {
   res.send("User Creted")
 })
 
 
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.orcjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function run() {
+  try{
+    client.connect()
+    const carCollection = client.db("carUser").collection("stokes");
+  }
+  catch{
+
+  }
+}
+run()
+
+
+
 // App Listen
 app.listen(port, () => {
-    console.log("O mai God ja Hard Assiment",port);
+  console.log("O mai God ja Hard Assiment", port);
 })
