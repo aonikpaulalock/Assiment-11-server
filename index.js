d4d5115a7bbc8c337be8ed1dd9654c5b21c77d76
aonikpaulalock <alockpaul9845@gmail.com>
2022-05-03 21:28:10 +0600

    add deploy api

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(express.json())
 require('dotenv').config()
 
 app.get("/", (req, res) => {
-  res.send("User Creted")
+  res.send("MataNosto Assiment")
 })
 
-
+// Client-Connect
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.orcjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
